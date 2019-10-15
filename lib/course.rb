eaccb277a8aad60ed683d99b19027b607f57b3d5
Charity Parks <tuppercharity@gmail.com>
2019-10-15 00:53:27 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,12 +1,12 @@
 class Course
   
-  #attr_accessor :title
+  attr_accessor :title, :schedule, :description
   
-  def initialize (title)
-    @title = title
-  end
+  # def initialize (title)
+  #   @title = title
+  # end
   
-  def title
-    @title
-  end
+  # def title
+  #   @title
+  # end
 end
